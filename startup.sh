90080447bfb64a2b9cf2ab70b084c0558194c3c1
LEETAEHO <tae1560@gmail.com>
2012-08-09 01:49:27 +0900

    Modified startup.sh miss connect bug

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@
 filepath=$0
 folderpath=${filepath%/*}
 host_name=$(hostname)
-$folderpath/$host_name/setip.sh
+$folderpath/$host_name/startup.sh
 
 #pvfs module loading
 insmod /lib/modules/`uname -r`/kernel/fs/pvfs2/pvfs2.ko
