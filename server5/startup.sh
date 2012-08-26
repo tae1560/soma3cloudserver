9dc195d8a9c9fb7f80ece8a99a527af98dadaf55
LEETAEHO <tae1560@gmail.com>
2012-08-26 13:10:53 +0900

    Add deaemon

--- server5/startup.sh
+++ server5/startup.sh
@@ -14,3 +14,14 @@ $folderpath/setmodules.sh
 # virtual ip binding for load balancing
 #$folderpath/../setip.sh bond0:0 10.12.17.200
 $folderpath/../setip.sh eth1:0 10.12.17.200
+
+# start pvfs2 server
+pvfs2-server -a 10.12.17.218 /etc/pvfs2-fs.conf
+
+# start pvfs2 client
+pvfs2-client -p ./pvfs2-client-core
+
+# mount 
+pvfs2fuse /mnt/storage-server1 -o fs_spec="tcp://10.12.17.214:3334/pvfs2-fs" -o allow_other
+pvfs2fuse /mnt/storage-server2 -o fs_spec="tcp://10.12.17.216:3334/pvfs2-fs" -o allow_other
+pvfs2fuse /mnt/storage-server3 -o fs_spec="tcp://10.12.17.218:3334/pvfs2-fs" -o allow_other
