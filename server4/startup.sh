7d91113b7bae863e9c2cadcaf1ce9bc4415b577d
LEETAEHO <tae1560@gmail.com>
2012-09-08 11:06:55 +0900

    edit script

--- server4/startup.sh
+++ server4/startup.sh
@@ -23,9 +23,9 @@ insmod /lib/modules/`uname -r`/kernel/fs/pvfs2/pvfs2.ko
 /usr/local/sbin/pvfs2-client -p /usr/local/sbin/pvfs2-client-core
 
 # mount 
-pvfs2fuse /mnt/storage-server1 -o fs_spec="tcp://10.12.17.214:3334/pvfs2-fs" -o allow_other
-pvfs2fuse /mnt/storage-server2 -o fs_spec="tcp://10.12.17.216:3334/pvfs2-fs" -o allow_other
-pvfs2fuse /mnt/storage-server3 -o fs_spec="tcp://10.12.17.218:3334/pvfs2-fs" -o allow_other
+/usr/local/bin/pvfs2fuse /mnt/storage-server1 -o fs_spec="tcp://10.12.17.214:3334/pvfs2-fs" -o allow_other
+/usr/local/bin/pvfs2fuse /mnt/storage-server2 -o fs_spec="tcp://10.12.17.216:3334/pvfs2-fs" -o allow_other
+/usr/local/bin/pvfs2fuse /mnt/storage-server3 -o fs_spec="tcp://10.12.17.218:3334/pvfs2-fs" -o allow_other
 
 #start logger
 cd $folderpath/../daemon
